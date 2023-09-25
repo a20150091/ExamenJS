c4c34b6e7ec057711ab2a355ce9e2ce789ee7eb5
a20150091 <131031875+a20150091@users.noreply.github.com>
2023-09-25 13:28:56 -0500

    Add files via upload

---
+++ evalua.js
@@ -0,0 +1,24 @@
+var nombre = prompt("Escribe tu nombre");
+var respuesta1 = prompt("1. ¿En qué departamento se encuentra el lago Titicaca?\nA:Tacna\nB: Puno\nC: Cusco");
+var respuesta2 = prompt("2. ¿Desde qué parte del Perú se pueden avistar ballenas jorobadas?\nA:Lima e Ica\nB: Arequiupa y Tacna\nC: Piura y Tumbes");
+var respuesta3 = prompt("3. Plato típico de la Selva peruana\nA: Tacacho con cecina\nB: Arroz con pollo\nC: Lomo saltado");
+
+
+
+var parseInt(puntaje)= 0
+
+if (respuesta1 == "B"){
+    var puntaje = puntaje + 25;
+}
+
+if (respuesta2 == "A"){
+    var puntaje = puntaje + 25;
+}
+
+if (respuesta3 == "C"){
+    var puntaje = puntaje + 25,
+}
+
+if (respuesta4 == "B"){
+
+}
